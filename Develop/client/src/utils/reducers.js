2de156ff1624093aea69d2bda6a47c652c5f1715
JD <jondavidperson@gmail.com>
2023-09-07 14:44:57 -0500

    added auth and reducers to utilities

---
+++ Develop/client/src/utils/reducers.js
@@ -0,0 +1,12 @@
+import { useReducer } from 'react';
+//import { actions } from './actions';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION:
+      return {
+        ...state,
+        
+      }
+  }
+}
