ddbdf9a93d6161f1707a0a8cb3f8f638e23aed18
dhm0010 <dhm0010@gmail.com>
2023-09-08 17:52:50 -0500

    got the API mostly working

--- Develop/client/src/components/Search/index.js
+++ Develop/client/src/components/Search/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 console.log("Plant imported");
-class Plant extends React.Component {
+class Search extends React.Component {
   // Constructor
   constructor(props) {
     super(props);
@@ -26,6 +26,7 @@ class Plant extends React.Component {
     )
       .then((res) => res.json())
       .then((json) => {
+        console.log("API Response: ", json);
         this.setState({
           items: [json],
           DataisLoaded: true,
@@ -34,6 +35,7 @@ class Plant extends React.Component {
   }
   render() {
     const { DataisLoaded, items } = this.state;
+
     return (
       <div className="App">
         <input
@@ -42,13 +44,29 @@ class Plant extends React.Component {
           onChange={this.handleInputChange}
         />
         <button onClick={this.fetchData}>Submit</button>
+
         {DataisLoaded &&
-          items.map((item) => (
-            <ol key={item.id}>Common Name: {item.common_name}</ol>
+          items.map((item, index) => (
+            <div key={index}>
+              <ol>Common Name: {item.common_name}</ol>
+              <ol>Scientific Name: {item.scientific_name}</ol>
+              <ol>Watering: {item.watering}</ol>
+              <ol>Sunlight: {item.sunlight}</ol>
+              <ol>Cycle: {item.cycle}</ol>
+              <ol>Growth Rate: {item.growth_rate}</ol>
+              <ol>Maintenance: {item.maintenance}</ol>
+              <ol>Hardiness: {item.hardiness.min}</ol>
+              <ol>Edible Fruit: {item.edible_fruit ? "Yes" : "No"}</ol>
+              {item.default_image.medium_url ? (
+                <img src={item.default_image.medium_url} alt="Plant" />
+              ) : (
+                "Image not available"
+              )}
+            </div>
           ))}
       </div>
     );
   }
 }
 
-export default Plant;
+export default Search;
