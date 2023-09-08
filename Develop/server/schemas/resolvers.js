91017af59c02076df60887943f4088102142249b
JD <jondavidperson@gmail.com>
2023-09-08 11:51:24 -0500

    added mutation for watering, updating, and deleting a plant, added mutation for deleting a user

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -26,30 +26,6 @@ const resolvers = {
 
       return { token, user };
     },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
     addPlant: async (parent, { common_name, scientific_name, watering, sunlight, description }, context) => {
       if (context.user) {
         const newPlant = await Plant.create({
@@ -68,6 +44,106 @@ const resolvers = {
         return newPlant;
     }
       throw new AuthenticationError('Incorrect credentials');
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    updatePlant: async (parent, args) => {
+      const { _id, common_name, scientific_name, watering, sunlight } = args;
+
+      try {
+        const updatedPlant = await Plant.findByIdAndUpdate(
+          _id,
+          {
+            common_name,
+            scientific_name,
+            watering,
+            sunlight
+          },
+          { new: true }
+        );
+
+        if (!updatedPlant) {
+          throw new Error('Plant not found');
+        }
+
+        return updatedPlant;
+      } catch (error) {
+        throw new Error(`Error updating plant: ${error.message}`);
+      }
+    },
+    deleteUser: async (parent, args) => {
+      const { _id } = args;
+
+      try {
+        const deletedUser = await User.findByIdAndDelete(_id);
+
+        if (!deletedUser) {
+          throw new Error('User not found');
+        }
+
+        return { _id: deletedUser._id };
+      } catch (error) {
+        throw new Error(`Error deleting user: ${error.message}`);
+      }
+    },
+    deletePlant: async (parent, args) => {
+      const { _id } = args;
+
+      try {
+        const deletedPlant = await Plant.findByIdAndDelete(_id);
+
+        if (!deletedPlant) {
+          throw new Error('Plant not found');
+        }
+
+        return { _id: deletedPlant._id };
+      } catch (error) {
+        throw new Error(`Error deleting plant: ${error.message}`);
+      }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addWateringEvent: async (parent, {input}) => {
+      const { plantId, date, watered } = input;
+
+      try {
+        const plant = await Plant.findById(plantId);
+
+        if (!plant) {
+          throw new Error('Plant not found');
+        }
+
+        plant.wateringHistory.push({
+          date: new Date(date),
+          watered
+        });
+
+        await plant.save();
+
+        return plant;
+      } catch (error) {
+        throw new Error(`Error adding water: ${error.message}`);
+      }
     }
   }
 };
