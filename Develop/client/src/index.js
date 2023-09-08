288d1886d109c18f53eb441fb6aabedb586454fe
dhm0010 <dhm0010@gmail.com>
2023-09-08 13:52:48 -0500

    started messing with the API

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById("root");
+const appRoot = ReactDOM.createRoot(root);
+appRoot.render(<App />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
