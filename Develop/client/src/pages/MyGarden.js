94f2035e946deee505f8e2a89d9f0db509e11d48
Ana Segovia <anasegovia505@gmail.com>
2023-09-09 12:30:25 -0500

    Trying to fix routes erroring out

--- Develop/client/src/pages/MyGarden.js
+++ Develop/client/src/pages/MyGarden.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './MyGarden.css';
+import logo from './';
+import './';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { MyPlants } from "./components/MyPlants";
-import { Calender } from "./components/Calender";
-import { Footer } from "./components/Footer";
+import { NavBar } from "./components/MyGarden/NavBar";
+import { Banner } from "./components/MyGarden/Banner";
+import { MyPlants } from "./components/MyGarden/MyPlants";
+import { Calender } from "./components/MyGarden/Calender";
+import { Footer } from "./components/MyGarden/Footer";
 
 function App() {
   return (
