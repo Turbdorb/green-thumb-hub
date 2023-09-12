642a0d7b132f5167c4c9c73ea43a2ecf7b42d18d
JD <jondavidperson@gmail.com>
2023-09-12 11:30:58 -0500

    revert

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xlfikhl.mongodb.net/greenthumbhub`
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
