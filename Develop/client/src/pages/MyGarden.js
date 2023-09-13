2aa0e1c17121c6e842fb58b7dd21c0a08eb0cd29
Ana Segovia <anasegovia505@gmail.com>
2023-09-13 14:21:02 -0500

    added reminder to mygarden page

--- Develop/client/src/pages/MyGarden.js
+++ Develop/client/src/pages/MyGarden.js
@@ -1,6 +1,7 @@
 import Nav from "../components/Nav";
 import { Banner } from "../components/Banner/Banner";
 import { MyPlants } from "../components/MyPlants/MyPlants";
+import { Reminder } from "../components/Reminder/Reminder";
 import { Footer } from "../components/Footer/Footer";
 
 function MyGarden() {
